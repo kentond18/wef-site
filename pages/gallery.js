598af8b470d1013b04af5a59dac14ceff013cb1c
Kenton Duprey <kdtech18@gmail.com>
2021-10-17 15:55:24 +0100

    fixing boilerplate for gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,24 +1,30 @@
 import Head from "next/head";
-import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 import client from "../config/sanityClientConstructor";
 
-export default function Home({ contactInfo }) {
+const Gallery = ({ contactInfo }) => {
 	return (
-		<div className={styles.container}>
+		<div className="vh-100 d-flex flex-column">
 			<Head>
-				<title>Gallery - World Eye Foundation</title>
-				<meta name="description" content="World Eye Foundation" />
+				<title>Gallery- World Eye Foundation</title>
+				<meta
+					name="description"
+					content="Gallery - World Eye Foundation"
+				/>
 				<link rel="icon" href="/wef_icon.png" />
 			</Head>
-			<div className="vh-100 d-flex flex-column">
-				<NavBar active="gallery" info={contactInfo} />
 
-				<Footer active="gallery" info={contactInfo} />
-			</div>
+			<NavBar info={contactInfo} active="gallery" />
+
+			<div className=""></div>
+
+			<Footer info={contactInfo} active="gallery" />
 		</div>
 	);
-}
+};
+
+export default Gallery;
 
 export async function getStaticProps() {
 	const infoQuery = `*[_type == "contact"]{
